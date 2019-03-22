060a3b08744f895623f85fde38401da519cac40c
312554OliverByl <47400006+312554OliverByl@users.noreply.github.com>
2019-03-22 10:01:36 -0400

    Initial commit

---
+++ CubeGenerator.cs
@@ -0,0 +1,126 @@
+﻿/* March 22, 2019
+ * Oliver Byl
+ * Cube generator.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _312554Checkerboard
+{
+    public class CubeGenerator
+    {
+
+        // Generator settings.
+        public int CubeSize { get; set; }
+        public int TileSize { get; set; }
+        public int XOffset { get; set; }
+        public int YOffset { get; set; }
+        public int XScale { get; set; }
+        public int YScale { get; set; }
+
+        public CubeGenerator()
+        {
+            CubeSize = 0;
+            TileSize = 0;
+            XOffset = 0;
+            YOffset = 0;
+            XScale = 1;
+            YScale = 1;
+        }
+
+        public Canvas Generate()
+        {
+            Canvas result = new Canvas();
+
+            //Generate the top side.
+            RotateTransform topRotate = new RotateTransform();
+            topRotate.Angle = 45;
+            ScaleTransform topScale = new ScaleTransform();
+            topScale.ScaleY = 0.5;
+            TranslateTransform topTranslate = new TranslateTransform();
+            topTranslate.X = 400;
+            topTranslate.Y = 40;
+
+            TransformGroup topTransforms = new TransformGroup();
+            topTransforms.Children.Add(topRotate);
+            topTransforms.Children.Add(topScale);
+            topTransforms.Children.Add(topTranslate);
+            result.Children.Add(GenerateSide(topTransforms, Brushes.LightGray, true));
+
+            //Generate the left side.
+            SkewTransform leftSkew = new SkewTransform();
+            leftSkew.AngleY = 21.4;
+            ScaleTransform leftScale = new ScaleTransform();
+            leftScale.ScaleX = 0.71;
+            leftScale.ScaleY = 0.9;
+            TranslateTransform leftTranslate = new TranslateTransform();
+            leftTranslate.X = 173;
+            leftTranslate.Y = 153;
+
+            TransformGroup leftTransforms = new TransformGroup();
+            leftTransforms.Children.Add(leftSkew);
+            leftTransforms.Children.Add(leftScale);
+            leftTransforms.Children.Add(leftTranslate);
+            result.Children.Add(GenerateSide(leftTransforms, Brushes.DarkGray, false));
+
+            //Generate the right side.
+            SkewTransform rightSkew = new SkewTransform();
+            rightSkew.AngleY = -21.4;
+            ScaleTransform rightScale = new ScaleTransform();
+            rightScale.ScaleX = 0.71;
+            rightScale.ScaleY = 0.9;
+            TranslateTransform rightTranslate = new TranslateTransform();
+            rightTranslate.X = 400;
+            rightTranslate.Y = 265;
+
+            TransformGroup rightTransforms = new TransformGroup();
+            rightTransforms.Children.Add(rightSkew);
+            rightTransforms.Children.Add(rightScale);
+            rightTransforms.Children.Add(rightTranslate);
+            result.Children.Add(GenerateSide(rightTransforms, Brushes.Gray, true));
+
+            return result;
+        }
+
+        private Canvas GenerateSide(TransformGroup transformations, Brush whiteColor, bool reflectColors)
+        {
+            Canvas result = new Canvas();
+            result.RenderTransform = transformations;
+
+            for (int yTile = 0; yTile < CubeSize; yTile++)
+            {
+                for (int xTile = 0; xTile < CubeSize; xTile++)
+                {
+                    int xOffset = (yTile % 2 == 0 ? 0 : 1) + (reflectColors ? 0 : 1);
+                    Brush color = Brushes.Black;
+                    if ((xTile + xOffset) % 2 == 0)
+                        color = whiteColor;
+
+                    Rectangle rectangle = new Rectangle
+                    {
+                        Width = TileSize,
+                        Height = TileSize,
+                        Fill = color
+                    };
+                    Canvas.SetLeft(rectangle, xTile * TileSize);
+                    Canvas.SetTop(rectangle, yTile * TileSize);
+                    result.Children.Add(rectangle);
+                }
+            }
+
+            return result;
+        }
+    }
+}
